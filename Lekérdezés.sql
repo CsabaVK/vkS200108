fa38c6ca710805bceb71ba4e9b4e5ab09bc22301
Csaba <56678429+CsabaVK@users.noreply.github.com>
2020-01-08 09:17:49 +0100

    Update Lekérdezés.sql

--- Lekérdezés.sql
+++ Lekérdezés.sql
@@ -4,3 +4,8 @@ where ar = (select min(ar) from `f_nyomtato`)
 and n.modell = t.modell
 and szines like 'igen'
 
+
+
+select distinct orszag
+from `$hajo_osztalyok`
+where agyuk_szama > (select avg(agyuk_szama) from `$hajo_osztalyok`)
